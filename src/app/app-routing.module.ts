ca516575c214501409248bedc9d0d0be1b090c4f
TammyPaola <tpc.acosta@yavirac.edu.ec>
2021-08-02 18:02:03 -0500

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BookComponent} from   './book/book.component';
+import { AuthorComponent } from './author/author.component';
+import { MainComponent } from './main/main.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component:MainComponent },
+  {path:'books', component:BookComponent },
+  {path:'authors',component:AuthorComponent },
+  {path: 'not-found', component: NotFoundComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,31 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthorComponent } from './author/author.component';
+import { MainComponent } from './main/main.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { BookComponent } from './book/book.component';
+import { UnderMaintenanceComponent } from './under-maintenance/under-maintenance.component';
+import { AccessDeniedComponent } from './access-denied/access-denied.component';
+import {ButtonModule} from 'primeng/button';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthorComponent,
+    MainComponent,
+    NotFoundComponent,
+    BookComponent,
+    UnderMaintenanceComponent,
+    AccessDeniedComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
